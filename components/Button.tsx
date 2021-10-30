a5adee9a26c2b60f63aa2b08af791c1a4187a93b
Justin Morgan <justin.morgan@hey.com>
2021-10-30 07:17:19 -0700

    :sparkles:  Add intro section to index page

---
+++ components/Button.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import Link from 'next/link';
+
+export const OutlineButtonStyles =
+    'text-center border-white border-2 rounded-full py-3 px-8 hover:bg-blue-100 hover:text-gray-900 transition duration-200';
+
+interface ButtonProps {
+    children: React.ReactNode;
+    className?: string;
+    onClick: () => void;
+}
+const Button = ({ children, className, onClick = () => {} }: ButtonProps) => (
+    <button onClick={onClick} className={`${OutlineButtonStyles} ${className}`}>
+        {children}
+    </button>
+);
+
+interface ButtonLinkProps {
+    children: React.ReactNode;
+    className?: string;
+    to: string;
+}
+const ButtonInternalLink = ({ children, className, to }: ButtonLinkProps) => (
+    <Link href={to}>
+        <a className={`${OutlineButtonStyles} ${className}`}>{children}</a>
+    </Link>
+);
+const ButtonLink = ({ children, className, to }: ButtonLinkProps) => (
+    <a href={to} target="_blank" rel="noreferrer" className={`${OutlineButtonStyles} ${className}`}>
+        {children}
+    </a>
+);
+
+export { Button, ButtonLink, ButtonInternalLink };
---
+++ sections/IndexIntro.tsx
@@ -0,0 +1,14 @@
+import { OutlineButtonStyles } from '../components/Button';
+
+const IndexIntro = () => (
+    <>
+        <p className="text-7xl py-8 max-w-4xl leading-tight">
+            Curious developer who is passionate about building digital communities
+        </p>
+        <a href="#footer" className={`${OutlineButtonStyles}`}>
+            Contact Me
+        </a>
+    </>
+);
+
+export default IndexIntro;
