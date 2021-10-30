8f18b737d1ecd58b76e2277bf5beab0b638365ec
Justin Morgan <justin.morgan@hey.com>
2021-10-30 07:20:40 -0700

    :sparkles:  Add UnderConstruction component for placeholder

---
+++ components/UnderConstruction.tsx
@@ -0,0 +1,12 @@
+const UnderConstruction = () => (
+    <div className="text-gray-50 flex flex-col justify-center items-center text-center space-y-8 mt-24">
+        <h1 className="text-6xl font-bold">Under Construction</h1>
+        <h2 className="text-2xl font-bold">Please check back in a little bit.</h2>
+        <p className="leading-relaxed max-w-xl text-lg">
+            This site is under active construction. If you find this page, I am likely going to have content here
+            shortly. This text is just a placeholder to allow me to properly route from other sections of the site.
+        </p>
+    </div>
+);
+
+export default UnderConstruction;
