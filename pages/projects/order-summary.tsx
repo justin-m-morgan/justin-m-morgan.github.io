72751c39f41336d27ee781d9a3e894463d024244
Justin Morgan <justin.morgan@hey.com>
2021-10-30 07:22:21 -0700

    :sparkles:  Add ProductFeature project details

---
+++ pages/projects/order-summary.tsx
@@ -0,0 +1,21 @@
+import UnderConstruction from '../../components/UnderConstruction';
+import { ProjectPreviewProps } from '../../sections/ProjectPreview';
+
+export const OrderSummaryPreview: ProjectPreviewProps = {
+    caseStudyPath: 'order-summary',
+    name: 'Order Summary Card',
+    ghRepoUrl: 'https://github.com/justin-m-morgan/fem_order-summary-component',
+    hostedUrl: 'https://justin-m-morgan.github.io/fem_order-summary-component/',
+    previewImgUrl: '/imgs/projects/OrderSummary/screenshot_desktop.png',
+    previewImgHeight: 1274,
+    previewImgWidth: 1361,
+    description: 'A purely static implementation of a design challenge from Frontend Mentors.',
+};
+
+export default function ClockApp() {
+    return (
+        <div>
+            <UnderConstruction />
+        </div>
+    );
+}
