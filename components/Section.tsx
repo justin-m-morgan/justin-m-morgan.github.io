3863ffcc8b9c03f04179fdbbafcf6f9c588eeecc
Justin Morgan <justin.morgan@hey.com>
2021-11-01 07:27:09 -0700

    :wheelchair: (sections) Improve color contrast of section headings

--- components/Section.tsx
+++ components/Section.tsx
@@ -5,9 +5,9 @@ interface SectionProps {
     heading?: string;
 }
 const Section = ({ className, children, heading, id }: SectionProps) => (
-    <section id={id} className="text-white px-4 md:px-18 lg:px-48 pt-24">
+    <section id={id} className="text-white px-4 md:px-8 lg:px-48 pt-24">
         {heading && (
-            <h2 className={`text-2xl lg:text-4xl text-gray-700 font-bold uppercase tracking-widest pb-16 ${className}`}>
+            <h2 className={`text-2xl lg:text-4xl text-gray-400 font-bold uppercase tracking-widest pb-16 ${className}`}>
                 {headingDecorator(heading)}
             </h2>
         )}
