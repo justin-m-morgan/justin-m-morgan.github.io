9c23b5552f5914b5a7a0ef797bc371320868f7d4
Justin Morgan <justin.morgan@hey.com>
2021-11-01 08:01:26 -0700

    :lipstick: (intro) Improve visual heirarchy of CTA in intro

--- sections/IndexIntro.tsx
+++ sections/IndexIntro.tsx
@@ -2,11 +2,13 @@ import { ButtonInternalLink } from '../components/Button';
 
 const IndexIntro = () => (
     <>
-        <p className="text-4xl lg:text-7xl py-8 max-w-xl lg:max-w-4xl font-bold leading-tight">
+        <p className="text-4xl md:text-5xl lg:text-7xl py-8 max-w-xl lg:max-w-4xl font-bold leading-tight">
             Curious developer who is passionate about building digital communities
         </p>
 
-        <ButtonInternalLink to="#footer">Contact Me</ButtonInternalLink>
+        <div className="flex justify-center md:justify-start mt-8 md:mt-12">
+            <ButtonInternalLink to="#footer">Contact Me</ButtonInternalLink>
+        </div>
     </>
 );
 
