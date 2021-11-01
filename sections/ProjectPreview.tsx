39a2bb68e7ad6440ae754fc09681a75f856cfd3f
Justin Morgan <justin.morgan@hey.com>
2021-11-01 06:58:43 -0700

    :recycle: (project-preview) Replace react-player with native video element

--- sections/ProjectPreview.tsx
+++ sections/ProjectPreview.tsx
@@ -1,7 +1,13 @@
 import Image from 'next/image';
-import { ButtonInternalLink, ButtonLink, OutlineButtonStyles } from '../components/Button';
-import ReactPlayer from 'react-player';
+import { ButtonInternalLink, ButtonLink } from '../components/Button';
 
+interface VideoPlayerProps {
+    className?: string;
+    src: string;
+}
+const VideoPlayer = ({ className, src }: VideoPlayerProps) => (
+    <video src={src} preload="auto" autoPlay={true} loop={true} muted={true} className={className}></video>
+);
 export interface ProjectPreviewProps {
     caseStudyPath: string;
     name: string;
@@ -26,27 +32,9 @@ const ProjectPreview = ({
     description,
 }: ProjectPreviewProps) => (
     <div className="grid lg:grid-cols-2 gap-8 mb-8">
-        <div className="rounded-lg overflow-hidden max-w-lg transform -translate-x-2 md: translate-x-0 mx-auto h-96 flex justify-center items-center">
+        <div className="rounded-lg overflow-hidden max-w-lg mx-auto flex justify-center items-center">
             {previewVideoUrl ? (
-                <ReactPlayer
-                    url={previewVideoUrl}
-                    playing={true}
-                    loop={true}
-                    muted={true}
-                    config={{
-                        file: {
-                            attributes: {
-                                style: {
-                                    height: '100%',
-                                    width: '100%',
-                                    objectFit: 'cover',
-                                    objectPosition: 'center center',
-                                    borderRadius: '8px',
-                                },
-                            },
-                        },
-                    }}
-                />
+                <VideoPlayer src={previewVideoUrl} className="rounded-lg overflow-hidden" />
             ) : (
                 <Image
                     src={previewImgUrl}
@@ -57,7 +45,7 @@ const ProjectPreview = ({
                 />
             )}
         </div>
-        <div className="flex flex-col place-content-center">
+        <div className="text-center flex flex-col place-content-center">
             <h3 className="text-2xl font-bold pb-4">{name}</h3>
             <p className="pb-8">{description}</p>
             <div className="flex justify-center space-x-4">
