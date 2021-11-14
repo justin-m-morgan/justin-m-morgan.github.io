e668ac2d8889b5b092256796dcc57258b30d17c0
Justin Morgan <justin.morgan@hey.com>
2021-11-14 13:48:19 -0800

    :iphone: (projects) Scale project template to mobile devices

--- components/ProjectCaseStudy.tsx
+++ components/ProjectCaseStudy.tsx
@@ -21,9 +21,9 @@ const ProjectCaseStudy = ({
     previewImgWidth,
     techUsed,
 }: ProjectCaseStudyProps) => (
-    <section className="container max-w-8xl mt-12 mx-auto grid grid-cols-2 gap-8 items-center">
+    <section className="px-2 container max-w-8xl mt-12 mx-auto flex flex-col md:grid md:grid-cols-2 gap-8 items-center">
         <div>
-            <h1 className="text-8xl font-bold text-center mt-24 pb-12">{name}</h1>
+            <h1 className="text-5xl md:text-8xl font-bold text-center mt-24 pb-12">{name}</h1>
             <div className="flex justify-center space-x-4">
                 <Github label="GitHub" url={ghRepoUrl} className="h-8" />
                 <ExternalLink url={hostedUrl} label="Live Site" className="h-8" />
@@ -44,15 +44,15 @@ const ProjectCaseStudy = ({
             )}
         </div>
 
-        <p className="text-xl leading-loose">{description}</p>
+        <p className="text-xl leading-relaxed md:leading-loose">{description}</p>
 
         <div>
-            <h2 className="text-4xl font-bold pb-4">Technology Used</h2>
+            <h2 className="text-2xl md:text-4xl font-bold pb-4">Technology Used</h2>
             <div className="flex flex-col space-y-3">
                 {techUsed.map(([icon, label]) => (
                     <div key={label} className="flex space-x-4 items-center">
                         <SymbolIcon icon={icon} className="w-8 h-8" />
-                        <span className="font-bold text-xl">{label}</span>
+                        <span className="font-bold  md:text-xl">{label}</span>
                     </div>
                 ))}
             </div>
