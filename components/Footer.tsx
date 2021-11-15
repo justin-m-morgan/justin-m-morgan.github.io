d328ff585a14ebae8f52cbec149e650af7b88ee8
Justin Morgan <justin.morgan@hey.com>
2021-11-15 10:35:03 -0800

    :lipstick: (footer) Fix wonky alignment and simplify layout styles

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -18,29 +18,29 @@ const icons = [Twitter, Linkedin, Github, Dev];
 const Footer = () => {
     const [isOpen, setIsOpen] = useState(false);
     return (
-        <>
-            <footer
-                id="footer"
-                className="px-24 md:px-8 lg:px-36 xl:px-48 max-w-8xl mx-auto pb-24 lg:pb-48 flex flex-col md:grid md:grid-cols-2 md:grid-rows-2 grid-flow-col items-center space-y-4 md:space-y-0 text-center"
-            >
-                <Modal title="Contact Me!" isOpen={isOpen} setIsOpen={setIsOpen}>
-                    <ContactForm />
-                </Modal>
+        <footer
+            id="footer"
+            className="container pb-24 lg:pb-48 
+                grid gap-4 md:grid-cols-2 md:grid-rows-2 grid-flow-col
+                text-center"
+        >
+            <Modal title="Contact Me!" isOpen={isOpen} setIsOpen={setIsOpen}>
+                <ContactForm />
+            </Modal>
 
-                <h4 className="text-center text-lg text-gray-50">Find me on any of these platforms</h4>
+            <h4 className="text-3xl font-bold text-gray-50">Find me on any of these platforms</h4>
 
-                <div className="lg:mb-0 flex justify-center space-x-4">
-                    {icons.map((Icon, i) => (
-                        <Icon key={i} />
-                    ))}
-                </div>
+            <div className="lg:mb-0 flex justify-center space-x-4">
+                {icons.map((Icon, i) => (
+                    <Icon key={i} />
+                ))}
+            </div>
 
-                <h4 className="text-lg">Or send me an email!</h4>
-                <div>
-                    <Button onClick={() => setIsOpen(true)}>Send Message</Button>
-                </div>
-            </footer>
-        </>
+            <h4 className="text-3xl font-bold">Or send me an email!</h4>
+            <div>
+                <Button onClick={() => setIsOpen(true)}>Send Message</Button>
+            </div>
+        </footer>
     );
 };
 
