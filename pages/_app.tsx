64047c906c4b5110d7021c132ef6929d229603ef
Justin Morgan <justin.morgan@hey.com>
2021-11-16 13:49:19 -0800

    :art: (favicon) Add favicons to head

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import React from 'react';
 import Head from 'next/head';
+import Script from 'next/script';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import { IconSymbols } from '../components/IconSymbols';
@@ -16,10 +17,13 @@ function MyApp({ Component, pageProps }: AppProps) {
                     name="description"
                     content="Professional Portfolio Site for Justin Morgan, a software developer in southern British Columbia"
                 />
-                <link
-                    rel="stylesheet"
-                    href="https://demos.creative-tim.com/notus-js/assets/vendor/@fortawesome/fontawesome-free/css/all.min.css"
-                />
+                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+                <link rel="manifest" href="/site.webmanifest" />
+                <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+                <meta name="msapplication-TileColor" content="#da532c" />
+                <meta name="theme-color" content="#ffffff"></meta>
                 <link rel="icon" href="/favicon.ico" />
             </Head>
             <IconSymbols />
