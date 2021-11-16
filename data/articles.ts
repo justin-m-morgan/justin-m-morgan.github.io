af0cf283966b10a66e1df33abdadf8ceca43d1ef
Justin Morgan <justin.morgan@hey.com>
2021-11-16 13:16:28 -0800

    :recycle: (data) Extract data from templates to dedicated directory

---
+++ data/articles.ts
@@ -0,0 +1,41 @@
+export interface DevToArticle {
+    id: number;
+    title: string;
+    description: string;
+    tag_list: string[];
+    canonical_url: string;
+    comments_count: number;
+    positive_reactions_count: number;
+    public_reactions_count: number;
+    page_views_count: number;
+    reading_time_minutes: number;
+}
+
+export const featuredArticles: DevToArticle[] = [
+    {
+        id: 638591,
+        title: 'Point-Free Style (in Javascript)',
+        description:
+            'Point-free style exploits function arity to allow for passing of paramater-less function references in interesting ways. But taken too far, it can be a challenge for the uninitiated to engage with code written in this style.',
+        comments_count: 0,
+        public_reactions_count: 10,
+        page_views_count: 561,
+        positive_reactions_count: 10,
+        tag_list: ['javascript', 'pointfree', 'functional'],
+        canonical_url: 'https://dev.to/justin_m_morgan/point-free-style-in-javascript-43o9',
+        reading_time_minutes: 13,
+    },
+    {
+        id: 549015,
+        title: 'A Deeper Dive into Function Arity',
+        description:
+            'Callbacks in Javascript have a great deal of flexibility. But have you ever taken the time to consider their various forms? We will explore how the concept of "arity" is often glossed over by Javascript developers.',
+        comments_count: 0,
+        public_reactions_count: 9,
+        page_views_count: 347,
+        positive_reactions_count: 9,
+        tag_list: ['javascript', 'arity', 'functional', 'cleancode'],
+        canonical_url: 'https://dev.to/justin_m_morgan/a-deeper-dive-into-function-arity-with-a-focus-on-javascript-ae',
+        reading_time_minutes: 10,
+    },
+];
---
+++ data/projects.ts
@@ -0,0 +1,6 @@
+import { ClockAppProject } from './projects/ClockApp';
+import { JobBoardProject } from './projects/JobBoard';
+import { OrderSummaryProject } from './projects/OrderSummary';
+import { Portfolio } from './projects/Portfolio';
+
+export { ClockAppProject, JobBoardProject, OrderSummaryProject, Portfolio };
