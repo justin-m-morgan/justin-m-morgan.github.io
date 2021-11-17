6f189a9221e40f0507272d2111b5b12d26a75c1e
Justin Morgan <justin.morgan@hey.com>
2021-11-17 09:24:28 -0800

    :speech_balloon: (project) Update portfolio case study text

--- data/projects/Portfolio.ts
+++ data/projects/Portfolio.ts
@@ -19,33 +19,33 @@ export const Portfolio: ProjectProps = {
     ],
     caseStudy: [
         {
-            heading: 'TailwindCSS',
+            heading: 'Consistent Styles with TailwindCSS',
             paragraphs: [
                 'TailwindCSS is an increasingly popular option to manage CSS. Its popularity arises from its "utility-class" approach to styling, which provides close-to-the-metal control of your styles.',
                 'With Tailwind, a global configuration file allows for centralized theming and customization. The recent addition of the just-in-time compiler allows for the addition of most pseudo-classes and elements.',
             ],
         },
         {
-            heading: 'NextJS',
+            heading: 'Pre-Rendered React with NextJS',
             paragraphs: [
                 'NextJS offers a pre-compiled solution for building React apps, resulting in faster initial renders.',
                 'Though not explored in this project, Next also supports a Server Side Generated mode for building server rendered React applications.',
             ],
         },
         {
-            heading: 'Headless UI',
+            heading: 'Accessibile Components with Headless UI',
             paragraphs: [
                 "Headless UI is a component library which is without styling-decisions. This makes it a perfect match for use with TailwindCSS, as headless can handle all the behaviours and accessibility concerns but the look and feel can perfectly mesh with your project's custom styles.",
             ],
         },
         {
-            heading: 'Husky CI',
+            heading: 'Pre-Commit CI Pipeline with Husky CI',
             paragraphs: [
                 'Using Husky CI along with git-staged enforces passing of prettier and es-lint prior to committing.',
             ],
         },
         {
-            heading: 'Vercel CD',
+            heading: 'Continuous Deployment with Vercel',
             paragraphs: [
                 "Vercel's offers GitHub hook integrations, deploying new commits to the main branch. Additionally, Vercel offers preview deploys on a separate URL.",
             ],
