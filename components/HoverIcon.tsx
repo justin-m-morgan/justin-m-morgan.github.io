a1c87afcfc6e296fa5bdeb135272382fb5e16cc5
Justin Morgan <justin.morgan@hey.com>
2021-11-24 17:34:56 -0800

    :lipstick: (tailwind) Add xxs font size and shrink hover icon text

--- components/HoverIcon.tsx
+++ components/HoverIcon.tsx
@@ -7,7 +7,7 @@ interface HoverIconProps {
 const HoverIcon = ({ icon, label }: HoverIconProps) => (
     <div className="flex flex-col items-center group transform hover:scale-150 transition-transform duration-150">
         <SymbolIcon key={icon} icon={icon} className="w-8 h-8" />
-        <span className="text-xs opacity-0 group-hover:opacity-100">{label}</span>
+        <span className="text-xxs opacity-0 group-hover:opacity-100 group-hover:scale-110">{label}</span>
     </div>
 );
 
